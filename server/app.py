d8d305e8a415e1b7e0ccce4da7df32eb4e5a22b7
Uwes Ahabab <uwesahabab@gmail.com>
2024-06-29 15:41:06 +0300

    ChatterBox

--- server/app.py
+++ server/app.py
@@ -13,14 +13,54 @@ CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
+@app.route("/")
+def index():
+    return "<h1>Hello, World!</h2>"
 
-@app.route('/messages')
+@app.route('/messages',methods=["GET","POST"])
 def messages():
-    return ''
-
-@app.route('/messages/<int:id>')
+    if request.method=="GET":  
+        messages=[message.to_dict() for message in Message.query.order_by(Message.created_at).all()]
+        response=make_response(jsonify(messages),200)
+        return response
+    elif request.method=="POST":
+        new_message=Message(
+            body=request.form.get('body'),
+            username= request.form.get('username')
+            
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        response=make_response(new_message.to_dict(),201)
+        return response
+        
+        
+    
+@app.route('/messages/<int:id>',methods=["GET","PATCH","DELETE"])
 def messages_by_id(id):
-    return ''
+    
+    message=Message.query.filter(Message.id==id).first()
+    if message is None:
+        response=make_response(jsonify({"error":"Message not found"}),404)
+        return response
+    else:
+        if request.method=="GET":
+            response=make_response(message.to_dict(),200)
+            return response
+        elif request.method=="PATCH":
+            for attr in request.form:
+                setattr(message,attr,request.form.get(attr))
+            db.session.add(message)
+            db.session.commit()
+            response=make_response(jsonify(message.to_dict()),200)
+            return response
+        elif request.method=="DELETE":
+            db.session.delete(message)
+            db.session.commit()
+            response=make_response(jsonify({"message":"Message deleted successfully"}),204)
+            return response
+        
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
