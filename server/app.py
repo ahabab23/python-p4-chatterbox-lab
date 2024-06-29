010025c46f314785bccd9aeaf8983cd1c528397b
Uwes Ahabab <uwesahabab@gmail.com>
2024-06-29 16:57:42 +0300

    CHATTERBOX-LAB

--- server/app.py
+++ server/app.py
@@ -13,54 +13,48 @@ CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
-@app.route("/")
-def index():
-    return "<h1>Hello, World!</h2>"
 
-@app.route('/messages',methods=["GET","POST"])
+@app.route('/')
+def home():
+    return '<h1>Messages</h1>'
+
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    if request.method=="GET":  
-        messages=[message.to_dict() for message in Message.query.order_by(Message.created_at).all()]
-        response=make_response(jsonify(messages),200)
-        return response
-    elif request.method=="POST":
-        new_message=Message(
-            body=request.form.get('body'),
-            username= request.form.get('username')
-            
+    # messages = [message.to_dict() for message in Message.query.order_by(Message.created_at.asc()).all()]
+    # return make_response(messages, 200)
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.order_by(Message.created_at.asc()).all()]
+        return make_response(messages, 200)
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
         )
         db.session.add(new_message)
         db.session.commit()
-        response=make_response(new_message.to_dict(),201)
-        return response
-        
-        
-    
-@app.route('/messages/<int:id>',methods=["GET","PATCH","DELETE"])
+        return make_response(new_message.to_dict(), 201)
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    
-    message=Message.query.filter(Message.id==id).first()
-    if message is None:
-        response=make_response(jsonify({"error":"Message not found"}),404)
-        return response
-    else:
-        if request.method=="GET":
-            response=make_response(message.to_dict(),200)
-            return response
-        elif request.method=="PATCH":
-            for attr in request.form:
-                setattr(message,attr,request.form.get(attr))
-            db.session.add(message)
-            db.session.commit()
-            response=make_response(jsonify(message.to_dict()),200)
-            return response
-        elif request.method=="DELETE":
-            db.session.delete(message)
-            db.session.commit()
-            response=make_response(jsonify({"message":"Message deleted successfully"}),204)
-            return response
-        
-    
+        message = Message.query.filter_by(id=id).first()
+        if message == None:
+            return make_response({"message": "Message not found"}, 404)
+        else:
+            if request.method == 'GET':
+                return make_response(message.to_dict(), 200)
+            elif request.method == 'PATCH':
+                data = request.get_json()
+                for attr in data:
+                    setattr(message, attr, data[attr])
+                    db.session.add(message)
+                    db.session.commit()
+                    return make_response(message.to_dict(), 200)
+                
+            elif request.method == 'DELETE':
+                db.session.delete(message)
+                db.session.commit()
+                return make_response({'message': 'record successfully deleted'}, 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
